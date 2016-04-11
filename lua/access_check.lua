9aef667ea2b48dfd54ad712844f048734ed213d5
Kane <kane@geeyo.com>
2016-04-11 16:48:54 +0800

    add access_check function and can do api ACL checking

---
+++ lua/access_check.lua
@@ -0,0 +1,45 @@
+local config = require("appConfig")
+
+if config["acl_check_enabled"] ~= 'Y' then
+  return
+end
+
+local utils = require "lib.utils"
+local cjson = require "cjson"
+
+local args
+
+if (ngx.var.request_method == "POST") then
+	ngx.req.read_body()
+   args = ngx.req.get_post_args()
+else
+   args = ngx.req.get_uri_args()
+end
+
+
+if not args.access_token then
+  ngx.say(cjson.encode(utils.getReturnResult("71","sorry, without access_token parameter")))
+  return
+end
+
+local httpt = require "resty.http"
+local httpc = httpt.new()
+local res, err = httpc:request_uri(config["acl_check_url"], {
+  method = "POST",
+  body = "access_token=" .. args.access_token .. "&api_url=" .. ngx.var.request_uri,
+  headers = {
+    ["Content-Type"] = "application/x-www-form-urlencoded",
+  }
+})
+
+if not res then
+  ngx.say(cjson.encode(utils.getReturnResult("72","Error when do auth checking: " .. err)))
+  return
+end
+
+local jsonData = cjson.decode(res.body)
+
+if jsonData.errorCode ~= '00' then
+  ngx.say(cjson.encode(utils.getReturnResult("73","Error when validating access_token: " .. jsonData.errorMessage)))
+  return
+end
--- lua/appConfig.lua
+++ lua/appConfig.lua
@@ -5,4 +5,8 @@ _m["redis_port"] = 6379
 _m["redis_require_auth"] = "N"
 _m["redis_auth_pass"] = "the_password_of_redis"
 
+-- "Y" means enabling acl checking
+_m["acl_check_enabled"] = "N"
+_m["acl_check_url"] = "http://xxx.xxx.xxx.xxx/ESBServer/sysApi/checkACL"
+
 return _m
